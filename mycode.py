79e99bd4ae0a12a1dba6cf598ea9450e8aab832c
James <jherma12@serenity.emich.edu>
2018-11-19 11:15:37 -0500

    Initial Commit

---
+++ mycode.py
@@ -0,0 +1,7 @@
+#this is my code
+#Git is a version control software
+# -make changes
+# -add changes
+# -commit changes
+# -push changes to a repository
+#    This is how Git works although the repositiry is a optional step
